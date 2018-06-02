2ae3ae72c835377f4694518d8b181391815d606a
Jules978 <vanDrunen.julia@gmail.com>
2018-06-02 12:34:36 +0200

    more function layout

--- src/Game.java
+++ src/Game.java
@@ -9,31 +9,45 @@
  * @author jdrunen
  */
 public class Game {
-    
+
     Player player;
     Dice dice;
+    ScoreCalculator scoreCalculator;
     int turnsLeft;
-    int rerollsLeft;
-    
+
     Game(Player player) {
         this.player = player;
         this.dice = new Dice();
+        this.scoreCalculator = new ScoreCalculator();
         this.turnsLeft = 10; //or how many turns there are in a yahtzee game;
-        this.rerollsLeft = 3; //decreases after a reroll, resets during a new turn
-        
-        }
-    
-    public void newTurn(){
-    this.rerollsLeft = 3;
-    this.turnsLeft --;
-    dice.roll5(); //dice array = this return value;
-    //print dice
+
+    }
+
+    public void newTurn() {
+
+        this.turnsLeft--;
+        dice.roll5(); //dice array = this return value;
+        //print dice
         System.out.println("Do you want to reroll? Y/N");
         //if(reroll equals("Y"){
-        //  System.out.println("Which dice do you want to reroll?");
-        //  while reroll is true (break on 0, 6, 7, 8, 9) add numbers to array or arraylist
-       //}
-    
+        //this.reroll(dice array);
+        //}
+        // dice array = this return value (of the rerolled dice)
+
     }
-    
+
+    public void reroll() {
+        int rerollsLeft = 3;
+        // array or array list with 5 dice rolls;
+
+        while (rerollsLeft > 0) {
+            System.out.println("Which dice do you want to reroll?");
+            //   while reroll is true (break on 0, 6, 7, 8, 9) add numbers to array or arraylist
+            //   dice.reroll(rolled dice, diceToBeRerolled )   
+            //  
+        }
+        //int points = scoreCalculator.addition(dice array);
+        //player.addToScore(points);
+    }
+
 }
