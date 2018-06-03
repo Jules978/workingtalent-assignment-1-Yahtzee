4a184301c5fe45b699a9892512af3d95b3860e77
JJD <vanDrunen.julia@gmail.com>
2018-06-03 20:45:10 +0200

    it works, I think?!

--- src/Game.java
+++ src/Game.java
@@ -9,6 +9,7 @@
  * @author jdrunen
  */
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Game {
 
@@ -18,58 +19,67 @@ public class Game {
     int[] diceArray = new int[5];
 
     Game() {
+        
         this.player = player;
         this.dice = new Dice();
         this.scoreCalculator = new ScoreCalculator();
         this.player = new Player();
-
     }
 
     public void newTurn() {
-
+        
+        Scanner reader = new Scanner(System.in);
         this.diceArray = dice.roll5();
         int rerollsLeft = 3;
-        boolean wantsToReroll = true;
+        
+        
         for (int i = 0; i < this.diceArray.length; i++) {
             System.out.println("D1 =" + diceArray[i]);
         }
 
         while (rerollsLeft > 0) {
-            System.out.println("Do you want to reroll?");
             
-            if (wantsToReroll) {
+            System.out.println("You have " + rerollsLeft + " rerolls left");
+            System.out.println("Do you want to reroll? yes/no");
+            String answer = reader.nextLine();
+           
+            if (answer.equals("yes")) {
                 this.reroll();
                 System.out.println("Your dice:");
                 dice.printDice();
-                rerollsLeft --;
-            } else {break;}
-            
+                rerollsLeft--;
+            } else {
+                break;
+            }
 
         }
 
         int score = scoreCalculator.addition(this.diceArray);
         player.setScore(score);
-        System.out.println("you now have " + score + " points!" );
+        System.out.println("you now have " + score + " points!");
     }
 
     public void reroll() {
-
+        
+        Scanner reader = new Scanner(System.in);
         int[] rerolledDice = new int[5];
-        // array or array list with 5 dice rolls;
-
+        
         System.out.println("Which dice do you want to reroll?");
-      
 
         for (int i = 0; i < rerolledDice.length; i++) {
-            //which number? 1-6
-            //if number > 0 && <7
-            // number -1 
-            //add number to rerolledDice[i];
 
-            //if number = 0, continue (?)
+            System.out.print("Which die do you want to reroll? 1-6 to choose a die, enter 0 or >7 to get a reroll");
+            int number = Integer.parseInt(reader.nextLine());
+            if (number > 0 && number < 7) {
+                number--;
+                rerolledDice[i] = number;
+                System.out.println(rerolledDice);
+            } else {
+                break; //probably not needed, but whatever
+            }
+
         }
 
-        //   this.diceArray = dice.reroll(rolled dice, diceToBeRerolled )   
     }
 
 }
